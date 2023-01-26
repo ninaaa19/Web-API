1efd2684c8637bf938647c2692d4b157348f0348
ninaaa19 <nina.barth.huettlingen@gmail.com>
2023-01-26 15:54:57 +0100

    .

--- Webprojekt/backend/App.router.ts
+++ Webprojekt/backend/App.router.ts
@@ -3,8 +3,31 @@ import { AppMiddleware } from './App.middleware';
 
 const router: express.Router = express.Router();
 
-
 /*
+Beispiele Reiner:
+
+app.get('/', (req, res) => {
+    res.send('Got a GET request');
+});
+
+app.post('/', (req, res)=> {
+    res.send('Got a POST request');
+});
+
+app.put('/test', (req, res) => {
+    res.send('Got a PUT request');
+});
+
+app.delete('/test', (req, res) => {
+    res.send('Got a DELETE request')
+});
+
+app.listen(port, () => {
+    console.log('Running...');
+});
+
+
+----------------------------
 router.post();
 
 router.get;
