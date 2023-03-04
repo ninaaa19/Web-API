47caa50dbd809dfdf7878eff1e5d2a4d697db2d4
ninaaa19 <nina.barth.huettlingen@gmail.com>
2023-03-04 16:29:06 +0100

    Code Felix

--- Webprojekt/frontend/src/app/app.component.ts
+++ Webprojekt/frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -19,5 +20,16 @@ export class AppComponent implements OnInit {
     //this.http.get('http://localhost:8080/route', body as {}).subscribe((result: any) => {
       //return result;
     //})
+  }
+
+
+}
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(array: any[], key: string, value: string): any[] {
+    return array.filter(item => item[key].includes(value));
   }
 }
