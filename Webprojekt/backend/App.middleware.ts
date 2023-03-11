17c2408a89f2ced25648392632aaa3b0021e29e7
ninaaa19 <nina.barth.huettlingen@gmail.com>
2023-03-11 14:23:31 +0100

    backend

--- Webprojekt/backend/App.middleware.ts
+++ Webprojekt/backend/App.middleware.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 import { AppController } from './App.controller';
+import multer from "multer";
 export class AppMiddleware {
-
-
 
 
 static async test(req: express.Request, res: express.Response): Promise<void> {
@@ -25,7 +24,7 @@ static async test(req: express.Request, res: express.Response): Promise<void> {
 
     static async putobjekt(req: express.Request, res: express.Response): Promise<void> {
         try{
-            return res.status(200).end(JSON.stringify(await AppController.putobjekt(req.body.id, req.body.typ, req.body.titel, req.body.beschreibung, req.body.adresse, req.body.groesse, req.body.anzahlinteressent)));
+            return res.status(200).end(JSON.stringify(await AppController.putobjekt(req.body.id, req.body.typ, req.body.titel, req.body.beschreibung, req.body.adresse, req.body.groesse, req.body.anzahlinteressent, req.body.bild)));
         } catch(err) {
             console.error('test err', err);
             return res.status(500).end();
@@ -34,7 +33,7 @@ static async test(req: express.Request, res: express.Response): Promise<void> {
 
     static async postobjekt(req: express.Request, res: express.Response): Promise<void> {
         try{
-            return res.status(200).end(JSON.stringify(await AppController.postobjekt(req.body.id, req.body.typ, req.body.titel, req.body.beschreibung, req.body.adresse, req.body.groesse, req.body.anzahlinteressent)));
+            return res.status(200).end(JSON.stringify(await AppController.postobjekt(req.body.id, req.body.typ, req.body.titel, req.body.beschreibung, req.body.adresse, req.body.groesse, req.body.anzahlinteressent, req.body.bild)));
         } catch(err) {
             console.error('test err', err);
             return res.status(500).end();
@@ -43,7 +42,7 @@ static async test(req: express.Request, res: express.Response): Promise<void> {
 
     static async deleteobjekt(req: express.Request, res: express.Response): Promise<void> {
         try{
-            return res.status(200).end(JSON.stringify(await AppController.deleteobjekt(req.body.id, req.body.typ, req.body.titel, req.body.beschreibung, req.body.adresse, req.body.groesse, req.body.anzahlinteressent)));
+            return res.status(200).end(JSON.stringify(await AppController.deleteobjekt(req.body.id, req.body.typ, req.body.titel, req.body.beschreibung, req.body.adresse, req.body.groesse, req.body.anzahlinteressent, req.body.bild)));
         } catch(err) {
             console.error('test err', err);
             return res.status(500).end();
@@ -62,6 +61,36 @@ static async test(req: express.Request, res: express.Response): Promise<void> {
     static async filterobjekt(req: express.Request, res: express.Response): Promise<void> {
         try{
             return res.status(200).end(JSON.stringify(await AppController.sucheobjekt(req.body.id, req.body.typ, req.body.titel, req.body.beschreibung, req.body.adresse, req.body.groesse)));
+        } catch(err) {
+            console.error('test err', err);
+            return res.status(500).end();
+        }
+    }
+
+    
+    static async ehrhöheAnzahlI(req: express.Request, res: express.Response): Promise<void> {
+        try{
+            return res.status(200).end(JSON.stringify(await AppController.ehrhöheAnzahlI(req.body.id, req.body.anzahlinteressent)));
+        } catch(err) {
+            console.error('test err', err);
+            return res.status(500).end();
+        }
+    }
+    static async uploadImage(req: express.Request, res: express.Response): Promise<void> {
+        //Speicherort für hochgeladene Bilder
+        const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+        cb(null, "public/images");
+        },
+        filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
+        },
+        });
+
+  // Funktion zum Speichern des Bildes
+  const saveImage = multer({ storage }).single("image");
+        try{
+            return res.status(200).end(JSON.stringify(await AppController.uploadImage(req.body.id, req.body.typ, req.body.titel, req.body.beschreibung, req.body.adresse, req.body.groesse, req.body.anzahlinteressent)));
         } catch(err) {
             console.error('test err', err);
             return res.status(500).end();
